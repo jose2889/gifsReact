ee7aea50a98f19c545e3fc27c33029c070340b5c
Jose Hernandez <jose2889@gmail.com>
2020-06-23 23:54:47 -0400

    docs agregado

---
+++ docs/precache-manifest.6b5cea4e082b9c5aeb3bc28ffbf0b243.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73edc02fca522a322440283c5276f249",
+    "url": "/index.html"
+  },
+  {
+    "revision": "deb383f21f9d0e847f5b",
+    "url": "/static/css/main.58b71974.chunk.css"
+  },
+  {
+    "revision": "7df93c7276edf1b25086",
+    "url": "/static/js/2.65d56cdc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.65d56cdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deb383f21f9d0e847f5b",
+    "url": "/static/js/main.82588146.chunk.js"
+  },
+  {
+    "revision": "9d3929294d677b1b9ff6",
+    "url": "/static/js/runtime-main.aa9df6a3.js"
+  }
+]);
